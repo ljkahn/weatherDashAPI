a904400b66f534b1e8caded201f1b3a032fb084c
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-03 16:52:21 -0600

    added baseline lat and lon values to API url and added fetch function to return current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 
 //define variables for API key and url
-var apiKey = "4c52e8bbc2510e2372cef27ffca3a887";
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
 
-var city;
+var lat = "39.7392";
+var lon = "104.9903";
+var apiKey = "4c52e8bbc2510e2372cef27ffca3a887";
+var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
 
 
 
 
 fetch(apiUrl)
   .then(function (response) {
+    console.log(response);
     return response.json();
-  });
+  })
+
+
+  .then(function(data){
+    console.log(data);
+});
