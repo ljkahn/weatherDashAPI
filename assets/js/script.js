2bd94297718dc39d69f22121c9117ea82f81e06f
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-03 16:19:31 -0600

    added weather api url and function for response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 
 //define variables for API key and url
 var apiKey = "4c52e8bbc2510e2372cef27ffca3a887";
-// var apiUrl = "https://api.openweathermap.org/data/2.5/";
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
 
 var city;
 
-//create a query url to store the weather data url and other variables
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-//create a fettch function
-fetch(queryURL) 
-    .then(function (response) {
-        return response.json();
-      });
 
-      console.log(fetch);
+fetch(apiUrl)
+  .then(function (response) {
+    return response.json();
+  });
