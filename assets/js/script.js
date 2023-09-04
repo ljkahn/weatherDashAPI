9f679ddac67c43f2116782d17b6ad6560ce5c849
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-04 14:47:33 -0600

    added function to filter through date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,68 @@
 
-//define variables for API key, url, lat and lon
+//define variables for API key, url
 
-var lat = "39.7392";
-var lon = "104.9903";
+var city = "Monaco";
 var apiKey = "4c52e8bbc2510e2372cef27ffca3a887";
-var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+var apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=`;
 
 
-var fiveDayUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-
+var fiveDayUrl =`https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&q=`;
 
 
 
-fetch(apiUrl)
+//fetch current weather data from OpenWeather API
+
+function daily (city) {
+    fetch(apiUrl + city)
   .then(function (response) {
     console.log(response);
     return response.json();
   })
 
-
+//loop through OpenWeather data
   .then(function(data){
     console.log(data);
-
-    for (var i = 0; i <data.length; i++) { 
-        console.log(data[i].main.weather)};
 });
+}
 
 
-
-   fetch(fiveDayUrl)
+   function fiveDay (city) {
+    fetch(fiveDayUrl + city)
   .then(function (response) {
-    console.log(response);
+    // console.log(response);
+    
     return response.json();
   })
 
   .then(function(data){
-    console.log(data);
+    // console.log(data);
 
-    for (var i = 0; i <data.length; i++) { 
-        console.log(data[i].weather)};
-});
+    //create a variable 
+
+ 
+
+    let newArray = data.list.filter(function (el) {
+        var test = el.dt_txt.split(" ")[1];
+        return test === "12:00:00";
+    }
+    );
+    console.log(newArray);
 
 
+    for (var i = 0; i < data.list.length; i++) {
+        // Access specific properties within each item
+        var wind = data.list[i].wind;
+        var temperature = data.list[i].main.temp;
+        var humidity = data.list[i].main.humidity;
 
+
+              // Log the data for the current item
+    //   console.log("Wind", wind);
+    //   console.log("Temp", temperature);
+    //   console.log("Humidity", humidity);
+}});
+   }
+
+
+// daily(city);
+fiveDay(city);
